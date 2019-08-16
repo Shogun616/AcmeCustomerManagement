523c04ce12cb7e98e4d3f22f6369c7b4bd44bd5c
Philip Mattsson <maini.ac@hotmail.com>
2019-08-16 19:22:45 +0200

    Version 3

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,9 @@ namespace ACM.BL
     public class Customer
     {
         public int CustomerId { get; set; }
-        public int EmailAddress { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
         /// <summary>
         /// Calculates the percent of the step goal reached.
@@ -25,7 +26,7 @@ namespace ACM.BL
             if (string.IsNullOrWhiteSpace(actualSteps)) throw new ArgumentException("Actual steps count must be entered", "actualSteps");
 
             // Try 3
-            if (!decimal.TryParse(goalSteps, out decimal goalStepCount)) throw new ArgumentException("Goal must be numeric"/*, "goalSteps"*/);
+            if (!decimal.TryParse(goalSteps, out decimal goalStepCount)) throw new ArgumentException("Goal must be numeric");
             if (!decimal.TryParse(actualSteps, out decimal actualStepCount)) throw new ArgumentException("Actual steps must be numeric", "actualSteps");
 
             return CalculatePercentOfGoalSteps(goalStepCount, actualStepCount);
@@ -36,14 +37,39 @@ namespace ACM.BL
             if (goalStepCount <= 0) throw new ArgumentException("Goal must be greater than 0", "goalSteps");
             return (actualStepCount / goalStepCount) * 100;
         }
-        public void ValidateEmail()
+        public OperationResult ValidateEmail()
         {
-            // -- Send an email receipt --
-            // If the user requested a receipt
-            // Get the customer data
-            // Ensure a valid email address was provided.
-            // If not,
-            // request an email address from the user.
+            var op = new OperationResult();
+
+            if (string.IsNullOrWhiteSpace(this.EmailAddress))
+            {
+                op.Success = false;
+                op.AddMessage("Email Address is null");
+            }
+
+            if (op.Success)
+            {
+                var isValidFormat = true;
+
+                if (!isValidFormat)
+                {
+                    op.Success = false;
+                    op.AddMessage("Email Address is not in a correct format");
+                }
+            }
+
+            if (op.Success)
+            {
+                var isRealDomain = true;
+
+                if (!isRealDomain)
+                {
+                    op.Success = false;
+                    op.AddMessage("Email Address does not include a valid domain");
+                }
+            }
+
+            return op;
         }
     }
 }
--- Core.Common/EmailLibrary.cs
+++ Core.Common/EmailLibrary.cs
@@ -8,7 +8,7 @@ namespace Core.Common
 {
     public class EmailLibrary
     {
-        public void SendEmail(int emailAddress, string v)
+        public void SendEmail(string emailAddress, string v)
         {
             // If a valid email address is provided,
             // Send an email.
